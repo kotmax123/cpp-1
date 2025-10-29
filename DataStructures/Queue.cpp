27f073470cfa397da2134947d3d4a6769e3ebdf0
kotmax123 <maksimkotliar09@gmail.com>
2025-10-29 10:49:35 +0200

    queue array set up added

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
+	int size = 0;
+	std::cout << "enter queue size:" << std::endl;
+	std::cin >> size;
+	Queue* q = new Queue[size];
 	
 	return 0;
 }
 
+void initQueue(Queue* q, unsigned int size)
+{
+	q;
+}
