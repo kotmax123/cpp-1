a763d4bb0e48a90673acdec2fa40242e07627cc4
kotmax123 <maksimkotliar09@gmail.com>
2025-10-29 11:53:02 +0200

    new start: deleted main function

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -1,23 +1,6 @@
 #include "Queue.h"
 #include <iostream>
 
-int main()
-{
-	int size = 0;
-	std::cout << "enter queue size:" << std::endl;
-	std::cin >> size;
-	while (size <= 0)
-	{
-		std::cout << "unavailable queue size.\nenter queue size:" << std::endl;
-		std::cin >> size;
-	}
-	Queue* q = new Queue[size];
-	initQueue(q, size);
-
-	
-	return 0;
-}
-
 void initQueue(Queue* q, unsigned int size)
 {
 	int i = 0;
@@ -25,4 +8,9 @@ void initQueue(Queue* q, unsigned int size)
 	{
 		q[i].value = i + 1;
 	}
+}
+
+void cleanQueue(Queue* q)
+{
+
 }
