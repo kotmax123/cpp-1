35ccf2b2e1e622cc5c1667172f6de81bdf4addd3
kotmax123 <maksimkotliar09@gmail.com>
2025-10-29 18:37:54 +0200

    initQueue is declared

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -1,17 +1,25 @@
 #include "Queue.h"
 #include <iostream>
 
+int main()
+{
+	Queue* q = new Queue;
+	initQueue(q, 5);
+	q[1].value = 5;
+	std::cout << q[1].value << std::endl;
+
+	return 0;
+}
+
 void initQueue(Queue* q, unsigned int size)
 {
-	int i = 0;
-	for (i = 0; i < size; i++)
-	{
-		q[i].value = i + 1;
-	}
+	Queue* u = new Queue[size];
+	q = u;
+	u = nullptr;
 }
 void cleanQueue(Queue* q)
 {
-
+	
 }
 
 void enqueue(Queue* q, unsigned int newValue)
@@ -20,14 +28,15 @@ void enqueue(Queue* q, unsigned int newValue)
 }
 int dequeue(Queue* q) // return element in top of queue, or -1 if empty
 {
-
+	
+	return 0;
 }
 
 bool isEmpty(Queue* s)
 {
-
+	return false;
 }
 bool isFull(Queue* s)
 {
-
+	return false;
 }
