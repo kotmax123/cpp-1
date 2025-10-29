9eb38ab03787d9eddb6e78ea2aa9205709f6bce6
kotmax123 <maksimkotliar09@gmail.com>
2025-10-29 11:56:04 +0200

    added all undeclared functions in cpp file

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -9,8 +9,25 @@ void initQueue(Queue* q, unsigned int size)
 		q[i].value = i + 1;
 	}
 }
-
 void cleanQueue(Queue* q)
+{
+
+}
+
+void enqueue(Queue* q, unsigned int newValue)
+{
+
+}
+int dequeue(Queue* q) // return element in top of queue, or -1 if empty
+{
+
+}
+
+bool isEmpty(Queue* s)
+{
+
+}
+bool isFull(Queue* s)
 {
 
 }
